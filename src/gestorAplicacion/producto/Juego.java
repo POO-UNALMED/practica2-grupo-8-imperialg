3c1848d2ec4f545e6dddb9d223bbc6fbd93db81b
Anderson Gutierrez <69688234+Anderson-Gt@users.noreply.github.com>
2020-10-21 23:34:30 -0500

    asesoriaalejo

--- src/gestorAplicacion/producto/Juego.java
+++ src/gestorAplicacion/producto/Juego.java
@@ -1,10 +1,16 @@
 /*1). En las fuentes se incluirá la siguiente documentación:
-         Cabecera del archivo: funcionalidad del módulo, autores, componentes del módulo, etc.
-         Cabeceras en las clases, explicando su finalidad y describiendo las estructuras de datos definidas cuando
+         Cabecera del archivo: funcionalidad del módulo, autores, componentes del módulo, etc.
+         Cabeceras en las clases, explicando su finalidad y describiendo las estructuras de datos definidas cuando
         sean relevantes.
-         Cabeceras en los métodos, comentando su propósito y describiendo los parámetros de entrada/salida.
-         Comentarios en líneas de código de relevante interés o importancia.
-         Otros aspectos de interés a tener en cuenta por el profesor.*/
+         Cabeceras en los métodos, comentando su propósito y describiendo los parámetros de entrada/salida.
+         Comentarios en líneas de código de relevante interés o importancia.
+        Otros aspectos de interés a tener en cuenta por el profesor.*/
+// Autores:   - Santiago Franco Valencia
+//            - Anderson Elian Gutierrez
+//            - Santiago Valencia Mejía
+//            - Daniel Alejandro Giraldo
+// En este módulo se crea la clase Juego, así como sus métodos básicos (Get y Set), además se definen un conjunto de atributos,
+// los cuales almacenarán información acerca de cada Juego que se vaya registrando en la plataforma.
 package gestorAplicacion.producto;
 import java.util.Scanner;
 
@@ -16,9 +22,9 @@ public class Juego extends Producto {
     private int pegi;
     private String plataforma;
     private String genero;
-    private static ArrayList<Juego> listaJuegos = Datos.listaJuegos;
-    
+    private static ArrayList<Juego> listaJuegos = Datos.listaJuegos;   
 
+    // Se crean los métodos Get y Set de los atributos de la Clase Consola
     public int getPegi() {
         return pegi;
     }
@@ -99,6 +105,7 @@ public class Juego extends Producto {
 }*/
 
 
+    // Se crea el constructor de la clase Juego, con sus atributos como parámetros.
     public Juego(String nombre, boolean uso, float precio, int pegi, String plataforma, String genero) {
         super(nombre, uso, precio);
         this.pegi = pegi;
@@ -107,6 +114,7 @@ public class Juego extends Producto {
     }
     
 
+    // Se crea el toString de la clase Consola, el cual mostrará por pantalla el Juego y sus caracteristicas.
     @Override
     public String toString() {
         return getNombre() + " " + plataforma + getPrecio() ;
--- src/gestorAplicacion/producto/Producto.java
+++ src/gestorAplicacion/producto/Producto.java
@@ -1,10 +1,16 @@
 /*1). En las fuentes se incluirá la siguiente documentación:
-         Cabecera del archivo: funcionalidad del módulo, autores, componentes del módulo, etc.
-         Cabeceras en las clases, explicando su finalidad y describiendo las estructuras de datos definidas cuando
+        Cabecera del archivo: funcionalidad del módulo, autores, componentes del módulo, etc.
+        Cabeceras en las clases, explicando su finalidad y describiendo las estructuras de datos definidas cuando
         sean relevantes.
-         Cabeceras en los métodos, comentando su propósito y describiendo los parámetros de entrada/salida.
-         Comentarios en líneas de código de relevante interés o importancia.
-         Otros aspectos de interés a tener en cuenta por el profesor.*/
+         Cabeceras en los métodos, comentando su propósito y describiendo los parámetros de entrada/salida.
+         Comentarios en líneas de código de relevante interés o importancia.
+         Otros aspectos de interés a tener en cuenta por el profesor.*/
+// Autores:   - Santiago Franco Valencia
+//            - Anderson Elian Gutierrez
+//            - Santiago Valencia Mejía
+//            - Daniel Alejandro Giraldo
+// En este módulo se crea la clase asbtracta Producto, así como sus métodos básicos (Get y Set), además se definen un conjunto de atributos,
+// los cuales almacenarán información acerca de cada producto  que se vaya registrando en la plataforma.
 package gestorAplicacion.producto;
 import java.io.Serializable;
 import java.util.Scanner;
@@ -16,6 +22,8 @@ public abstract class Producto implements Serializable{
     private float precio;
     private Boolean uso;
    
+
+    // Se crean los métodos Get y Set de los atributos de la Clase Producto
 
     public float getPrecio() {
         return precio;
@@ -54,13 +62,14 @@ public abstract class Producto implements Serializable{
 
     public abstract String toString();
 
+    // Se crea el constructor de la clase periferico, con sus atributos como parámetros.
+
     public Producto(String nombre, boolean uso, float precio) {
         this.nombre = nombre;
         this.precio = precio;
         this.uso = uso;
     }
     public Producto(String nombre) {
-    	this.nombre=nombre;
+        this.nombre=nombre;
     }
-
 }
